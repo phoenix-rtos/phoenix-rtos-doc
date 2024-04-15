4a37a462560d973f444173a8a054bbf1144bf91f
adamdebek <adam.debek@phoenix-rtos.com>
2024-04-15 22:28:55 +0200

    sphinx: disable dark mode
    JIRA: CI-418

--- conf.py
+++ conf.py
@@ -21,6 +21,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 myst_heading_anchors = 3
+pygments_dark_style = 'tango'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -30,8 +31,20 @@ html_favicon = '_images/RTOS_sign.png'
 html_theme = 'furo'
 html_style = ['css/furo-phoenix.css', 'css/furo-extensions-phoenix.css']
 html_static_path = ['_static', '_images']
+# TODO: add dark mode support
 html_theme_options = {
 	"light_css_variables": {
+		"sidebar-caption-font-size": "100%",
+		"sidebar-item-font-size": "90%",
+		"sidebar-item-spacing-vertical": "0.4rem",
+		"sidebar-item-line-height": "1.1rem",
+		"sidebar-search-space-above": "0.1rem",
+		"sidebar-caption-space-above": "0.1rem",
+		"sidebar-tree-space-above": "0.5rem",
+		"sidebar-search-input-font-size": "95%",
+	},
+
+	"dark_css_variables": {
 		"sidebar-caption-font-size": "100%",
 		"sidebar-item-font-size": "90%",
 		"sidebar-item-spacing-vertical": "0.4rem",
