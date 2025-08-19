4c3f64f860b4295ceb76a283422f84d47f362ab9
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2025-08-19 13:24:04 +0200

    sphinx: use docsresources in main conf.py
    JIRA: CI-533

--- conf.py
+++ conf.py
@@ -3,6 +3,7 @@
 from version_management import get_version_context
 from pathlib import Path
 from datetime import datetime
+from phoenixsystems.docsresources import latex_default
 
 project = ""
 copyright = "2024-2025, Phoenix Systems"
@@ -19,6 +20,7 @@ latexpdf_title = "Phoenix-RTOS Documentation"
 latexpdf_author = ""
 latexpdf_date = datetime.today().strftime('%d-%m-%Y')
 latexpdf_version = "Ver. latest"
+latexpdf_filename = "phoenix-rtos-documentation"
 
 html_theme = "furo"
 html_title = "Phoenix-RTOS Documentation"
@@ -73,52 +75,22 @@ html_theme_options = {
     },
 }
 
-latex_engine = "xelatex"
-latex_table_style = ["colorrows"]
 latex_documents = [
-    ("index", "phoenix-rtos.tex", "Phoenix-RTOS Documentation", author, "howto", False),
+    ("index", f"{latexpdf_filename}.tex", latexpdf_title, author, "howto", False),
 ]
 
-latex_additional_files = [
-    "_static/images/pdf/companylogo.png",
-    "_static/images/pdf/last-page-image.png",
-    "_static/images/pdf/first-page-image1.png",
-    "_static/images/pdf/first-page-image2.png"
-]
+latex_engine = latex_default["engine"]
+latex_table_style = latex_default["table_style"]
 
-latex_elements = {
-    'sphinxsetup': r'pre_padding-right=6pt, pre_padding-left=0pt',
-    'makeindex': r'',
-    'papersize': r'a4paper',
-    'tableofcontents': r'''
-        \makeatletter
-        \begingroup
-        \pagestyle{normal}
-        \section*{Table of Contents}
-        \@starttoc{toc}
-        \clearpage
-        \endgroup
-        \makeatother
-    ''',
-    'fontpkg': r'''
-        \setmainfont{Liberation Sans}
-        \setsansfont{Liberation Sans}
-        \setmonofont{DejaVu Sans Mono}
-    ''',
-    'extrapackages': r'''
-        \usepackage{tocloft}
-        \usepackage{graphicx}
-        \usepackage{xcolor}
-        \usepackage{etoolbox}
-        \usepackage{lastpage}
-        \usepackage{fontspec}
-    ''',
-    'preamble': Path("_static/latex/preamble.tex").read_text(),
-    'atendofbody': Path("_static/latex/atendofbody.tex").read_text(),
-    'maketitle': fr'''
+latex_additional_files = latex_default["additional_files"]
+
+# overwrite the maketitle element to use the variables from the current file
+modified_elements = latex_default["elements"].copy()
+modified_elements["maketitle"] = fr'''
         \newcommand{{\doctitle}}{{{latexpdf_title}}}
         \newcommand{{\docauthor}}{{{latexpdf_author}}}
         \newcommand{{\docversion}}{{{latexpdf_version}}}
         \newcommand{{\docdate}}{{{latexpdf_date}}}
-    ''' + Path("_static/latex/maketitle.tex").read_text()
-}
+    ''' + modified_elements["maketitle"]
+
+latex_elements = modified_elements
