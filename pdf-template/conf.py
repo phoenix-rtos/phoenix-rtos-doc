d6518b46154092972212421546977f3379d08137
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2026-01-13 12:57:05 +0100

    pdf_template: raise error when latexpdf_filename is in wrong format
    Sphinx does not accept filenames with spaces, but the error message in such cases is unclear.
    This has caused unnecessary debugging effort in several instances.
    
    JIRA: CI-626

--- pdf-template/conf.py
+++ pdf-template/conf.py
@@ -1,5 +1,6 @@
 # TemplateConfiguration file for the Sphinx documentation builder.
 
+import sys
 from datetime import datetime
 from phoenixsystems.docsresources import latex_default
 
@@ -16,6 +17,18 @@ latexpdf_author = "Your Name"
 latexpdf_date = datetime.today().strftime('%d-%m-%Y')
 latexpdf_version = "Ver. X.X"
 latexpdf_filename = "your_filename"
+
+if ' ' in latexpdf_filename:
+    print(
+        "\n" + "=" * 70 + "\n"
+        "ERROR: PDF filename cannot contain spaces!\n"
+        f"Current name: '{latexpdf_filename}'\n"
+        "Use underscores '_' or hyphens '-' instead of spaces.\n"
+        "Examples: 'my_documentation' or 'my-documentation'\n"
+        + "=" * 70 + "\n",
+        file=sys.stderr
+    )
+    sys.exit(1)
 
 latex_documents = [
     ("index", f"{latexpdf_filename}.tex", latexpdf_title, author, "howto", False),
