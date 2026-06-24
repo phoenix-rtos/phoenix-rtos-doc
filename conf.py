7b15c9a4ce94df6856500b8564c49a6a0b142e3a
Adam Greloch <adam.greloch@phoenix-rtos.com>
2026-06-24 08:52:21 +0200

    sphinx: enable "attrs_inline" extension
    Allows to specify inline attributes. Useful for icons/symbols.
    
    TASK: RTOS-1259

--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@ language = "en"
 
 extensions = ["myst_parser", "sphinx_copybutton", "phoenixsystems.docsresources"]
 
-myst_enable_extensions = ["deflist", "fieldlist"]
+myst_enable_extensions = ["deflist", "fieldlist", "attrs_inline"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["README", "_build", "Thumbs.db", ".DS_Store", "_venv", "docsresources", "pdf-template"]
